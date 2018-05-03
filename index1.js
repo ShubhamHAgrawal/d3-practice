0ebb40919d86bea5e7dec012be3b3e7984e28482
ShubhamHAgrawal <shubham5486@gmail.com>
2018-05-03 16:07:22 +0530

    Start with d3

---
+++ index1.js
@@ -0,0 +1,18 @@
+var nameIt = function() {
+d3.select("body")
+  .transition()
+  .style("background-color", function(){
+    return "hsl(" + Math.random() * 360 + ", 90%, 80%)"});
+d3.selectAll("p")
+  .style("color", function(){
+    return "hsl(" + Math.random() * 360 + ", 100%, 50%)"});
+}
+
+setInterval(nameIt, 100)
+
+d3.select('button')
+  .on('click', function () {
+    d3.select('body')
+      .append('h3')
+      .text('Today is a beautiful day!!');
+    });
